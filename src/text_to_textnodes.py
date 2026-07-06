6ef69674644fca8a9d953e8f3b6acc3bcb660f2b
sdlck <stevenking450@gmail.com>
2026-07-06 15:06:59 +0800

    update text_to_textnodes

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -3,8 +3,8 @@ from split_nodes import split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 def text_to_textnodes(text: str) -> list[TextNode]:
-    node = TextNode(text, TextType.TEXT)
-    nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_image(nodes)
