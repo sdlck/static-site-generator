727c16f4027a0f993ceec1aea127e6e46318274a
sdlck <stevenking450@gmail.com>
2026-07-06 13:36:08 +0800

    add extract_markdown and tests

---
+++ src/extract_markdown.py
@@ -0,0 +1,8 @@
+import re
+
+
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text:str) -> list[tuple[str, str]]:
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
