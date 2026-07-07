7ce41713e4d66fe236f682974d779cd6b0ce5b9e
sdlck <stevenking450@gmail.com>
2026-07-07 16:58:25 +0800

    add generate_pages_recursive

--- src/generate_page.py
+++ src/generate_page.py
@@ -23,3 +23,30 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
 
     with open(dest_path, "w") as f:
         f.write(template)
+
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    for object in os.listdir(dir_path_content):
+        path = os.path.join(dir_path_content, object)
+
+        if os.path.isfile(path) and path.endswith(".md"):
+            with open(template_path, "r") as f:
+                template = f.read()
+
+            with open(path, "r") as f:
+                md = f.read()
+            
+            node = markdown_to_html_node(md)
+            html = node.to_html()
+            title = extract_title(md)
+
+            new_template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+            if dest_dir_path:
+                os.makedirs(dest_dir_path, exist_ok=True)
+
+            dest_path = os.path.join(dest_dir_path, f"{object[:-3]}.html")
+            with open(dest_path, "w") as f:
+                f.write(new_template)
+
+        elif os.path.isdir(path):
+            generate_pages_recursive(path, template_path, os.path.join(dest_dir_path, object))
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from copy_dir import copy_dir
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 
 def main():
     copy_dir("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    #generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
