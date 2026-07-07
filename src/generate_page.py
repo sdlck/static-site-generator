1a189451ee2e9f1f0adb5e41bea23de697b1ee84
sdlck <stevenking450@gmail.com>
2026-07-07 16:07:34 +0800

    add generate_page

---
+++ src/generate_page.py
@@ -0,0 +1,25 @@
+from extract_markdown import extract_title
+from markdown_to_html_node import markdown_to_html_node
+import os
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    with open(from_path, "r") as f:
+        md = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
+    
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    title = extract_title(md)
+
+    template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    dest_dir = os.path.dirname(dest_path)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    with open(dest_path, "w") as f:
+        f.write(template)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from copy_dir import copy_dir
+from generate_page import generate_page
 
 def main():
     copy_dir("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
